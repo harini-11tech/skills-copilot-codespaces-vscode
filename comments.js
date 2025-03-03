be7e490683bd4c98390eac96a920ce5f77263454
harini-11tech <harini.boda@mphasis.com>
2025-03-03 02:23:28 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,14 +1,6 @@
 // create web server
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-// create a route for get request
-app.get('/comments', (req, res) => {
-    res.send('Comments Page');
-});
-
-// start the server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+// create a new comment
+// read all comments
+// read a single comment
+// update a comment
+// delete a comment
