103032719f45f6e66dc83c7f990d3562a13871a1
harini-11tech <harini.boda@mphasis.com>
2025-03-03 02:35:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,9 @@
 // create web server
-// update a comment
-// delete a comment
-
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
-app.use(bodyParser.json());
-
-var comments = [
-    { id: 1, author: 'John', body: 'hello world' },
-    { id: 2, author: 'Bob', body: 'hi' },
-    { id: 3, author: 'Alice', body: 'nice to meet you' }
-];
-
-app.get('/comments', function(req, res) {
-    res.json(comments);
-});
-
-app.get('/comments/:id', function(req, res) {
-    var comment = comments.find(function(comment) {
-        return comment.id === Number(req.params.id);
-    });
-    res.json(comment);
-});
-
-app.post('/comments', function(req, res) {
-    var newComment = {
-        id: comments.length + 1,
+// http.createServer(function(req, res) {
+//     // set header
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     // set response
+//     res.write('Hello World!');
+//     // end response
+//     res.end();
+// }).listen(8080);
